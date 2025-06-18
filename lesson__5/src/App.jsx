d4c034da85845d1fef936e27f351e5110c1b06c0
AlexGreen <107873309+Alex-Green-Oleksiy@users.noreply.github.com>
2025-06-18 09:58:34 +0300

    add 5

---
+++ lesson__5/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import TasksDivider from './components/TasksDivider/TasksDivider'
+// import Converter from './components/Converter/Converter'
+// import CardsList from './components/Google/CardsList'
+// import { favoriteSources } from './data/1_data_google_favorite'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Template</h1>
+        <TasksDivider />
+        {/* =========== */}
+        {/* <Converter /> */}
+        {/* ========== */}
+        {/* <CardsList sourceList={favoriteSources} /> */}
+        {/* ============= */}
+      </div>
+    </>
+  )
+}
+
+export default App
